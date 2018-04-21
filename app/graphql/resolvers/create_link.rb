19f6115e0518d97f99f6773b18aec4cb32ba7835
Pedro Pazello <email@pedropazello.com>
2018-04-21 18:33:46 -0300

    Change createLink to get signed user

--- app/graphql/resolvers/create_link.rb
+++ app/graphql/resolvers/create_link.rb
@@ -1,13 +1,14 @@
 class Resolvers::CreateLink < GraphQL::Function
-	argument :description, !types.String
-	argument :url, !types.String
+  argument :description, !types.String
+  argument :url, !types.String
 
-	type Types::LinkType
+  type Types::LinkType
 
-	def call(_obj, args, _ctx)
-		Link.create!(
-			description: args[:description],
-			url: args[:url],
-		)
-	end
+  def call(_obj, args, ctx)
+    Link.create!(
+      description: args[:description],
+      url: args[:url],
+      user: ctx[:current_user]
+    )
+  end
 end
--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -1,7 +1,8 @@
 Types::LinkType = GraphQL::ObjectType.define do
-	name 'Link'
+  name 'Link'
 
-	field :id, !types.ID
-	field :url, !types.String
-	field :description, !types.String
+  field :id, !types.ID
+  field :url, !types.String
+  field :description, !types.String
+  field :postedBy, -> { Types::UserType }, property: :user
 end
